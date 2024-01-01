86ca1aa4dadb2a20ad1d2700e46b1b38faeaf31b
nguyenhuynhhuuduc <134940937+nguyenhuynhhuuduc@users.noreply.github.com>
2024-01-01 21:20:01 +0700

    update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-green-500">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -35,13 +35,12 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            Cooperate Together for Development.
           </p>
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-sky-500 to-indigo-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-blue-600 to-green-500"
             >
               Hire Me
             </Link>
@@ -61,14 +60,16 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.jpeg"
-              alt="hero image"
-              className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
+          <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
+            <div className="absolute w-full h-full rounded-full overflow-hidden">
+              <Image
+                src="/images/hero-image.jpeg"
+                alt="hero image"
+                className="object-cover w-full h-full"
+                width={500}
+                height={500}
+              />
+            </div>
           </div>
         </motion.div>
       </div>
